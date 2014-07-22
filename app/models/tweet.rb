6b3993d8d529a82bf25a3ecca2387af94b091cd9
shehryar <ali.shehryar@live.com>
2014-07-22 22:15:58 +0500

    model cleaned.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -17,40 +17,14 @@ class Tweet
         conv_result_to_hash = raw_results.to_h
         
         
-        
-        
         #########################################################################################################################
         refined_results = conv_result_to_hash[:statuses].collect{|r| r[:text]} 
         tweets_array = refined_results.to_a
         tweets_array.each {|tweet_text| Tweet.create! :text => tweet_text , :sentiment => sentiments.score(tweet_text)["pos"] }
-       
-        
-       
-        #puts tweets_array
         end
             
         #########################################################################################################################
         
-        
-        # tweets will be separated by \n when you filter them out of results
-        # all you will havwe to do in order to get them in an array would 
-        # variable_name.split("\n")
-        # Test code:
-        # Tweet.first shows the first stored tweet
-        # Tweet.first.update_attributes! :sentiment => "happy" used to update attribute in tweet once the correct sentiment has been matched
 
-       
-        ############################################################################################################################
-
-    
-      
-    
-            #tweets_array.each {|tweet_score| Tweet.update_attribute(:sentiment => sentiments.score(tweet_score)["pos"])}
-            
-                
-                
-    
-     ################################################################################################################################   
 end
 
-#Tweet.each {|tweet_score| Tweet.update_attribute(:sentiment => sentiments.score(tweet_score)["pos"])}
